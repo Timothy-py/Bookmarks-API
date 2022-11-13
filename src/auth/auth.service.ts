e3b24cdf66bf4ad2f8fa5e4e513a8e0d89c9ef36
Timothy-py <adeyeyetimothy33@gmail.com>
2022-11-13 21:13:14 +0100

    implemented signup service and added relation between user and bookmark model

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,6 @@
-import  { Injectable } from "@nestjs/common";
+import  { ForbiddenException, Injectable } from "@nestjs/common";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import * as argon from "argon2"
 import { PrismaService } from "../prisma/prisma.service"
 import { AuthDto } from "./dto";
 
@@ -7,8 +9,30 @@ export class AuthService{
     constructor(private prisma: PrismaService){}
 
     // signup logic
-    signup(dto: AuthDto) {
-        return 'Signup successfully'
+    async signup(dto: AuthDto) {
+        try {
+            // hash the password
+            const hash = await argon.hash(dto.password)
+
+            // create user
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash
+                }
+            })
+
+            return user
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError){
+                if(error.code === 'P2002'){
+                    throw new ForbiddenException('Credentials taken');
+                    
+                }
+            }
+
+            throw error;
+        }
     }
 
     // login logic
