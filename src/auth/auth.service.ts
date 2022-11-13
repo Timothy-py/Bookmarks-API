ffdb2ab6661fadd73247d009e45b08bcdb570454
Timothy-py <adeyeyetimothy33@gmail.com>
2022-11-13 19:50:48 +0100

    added validation to Auth dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,18 @@
 import  { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service"
+import { AuthDto } from "./dto";
 
 @Injectable()
 export class AuthService{
     constructor(private prisma: PrismaService){}
+
+    // signup logic
+    signup(dto: AuthDto) {
+        return 'Signup successfully'
+    }
+
     // login logic
     login() {
         return 'Signin successfully'
-    }
-
-    // signup logic
-    signup() {
-        return 'Signup successfully'
     }
 }
