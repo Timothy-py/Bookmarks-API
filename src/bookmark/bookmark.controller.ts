d52fe8d1ebff2b99dd4493d5b23ebe8bed831236
Timothy-py <adeyeyetimothy33@gmail.com>
2022-12-05 23:32:18 +0100

    added return statement to bookmark controllers

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -13,29 +13,50 @@ export class BookmarkController {
     ){}
 
     @Get()
-    getBookmarks(@GetUser('id') userId: number) {}
+    getBookmarks(@GetUser('id') userId: number) {
+        return this.bookmarkService.getBookmarks(
+            userId
+        )
+    }
 
     @Get(':id')
     getBookmarksById(
         @GetUser('id') userId: number,
         @Param('id', ParseIntPipe) bookmarkId: number,
-    ) {}
+    ) {
+        return this.bookmarkService.getBookmarkById(
+            userId, bookmarkId
+        )
+    }
 
     @Post()
     createBookmark(
         @GetUser('id') userId: number,
         @Body() dto: createBookmarkDto,
-    ) {}
+    ) {
+        return this.bookmarkService.createBookmark(
+            userId, dto
+        )
+    }
 
-    @Patch()
+    @Patch(':id')
     editBookmarkById(
         @GetUser('id') userId: number,
+        @Param('id', ParseIntPipe) bookmarkId: number,
         @Body() dto: editBookmarkDto
-    ) {}
+    ) {
+        return this.bookmarkService.editBookmarkById(
+            userId, bookmarkId, dto
+        )
+    }
 
     @Delete(':id')
     deleteBookmarkById(
         @GetUser('id') userId: number,
         @Param('id', ParseIntPipe) bookmarkId: number
-    ) {}
+    ) {
+        return this.bookmarkService.deleteBookmarkById(
+            userId, bookmarkId
+        )
+    }
 }
